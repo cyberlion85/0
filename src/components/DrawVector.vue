901d4c4dcac36627b08f0fae6db6a015b6810ca5
spersanov <s.persanov@gmail.com>
2023-09-25 18:25:35 +0300

    vector small fix smoothing

--- src/components/DrawVector.vue
+++ src/components/DrawVector.vue
@@ -29,9 +29,9 @@ let currentPath: string[] = [];
 let pathStrings: string[] = reactive([]);
 let lastX: number | null = null;
 let lastY: number | null = null;
-let strokeWidth = ref(5); // Начальная толщина линии
+let strokeWidth = ref(5);
 
-const smoothingFactor = 0.2; // Устанавливаем фактор сглаживания, 0 - 1
+const smoothingFactor = 0.05;
 
 const svgRef = ref<SVGSVGElement | null>(null);
 
@@ -40,25 +40,27 @@ const handleMouseDown = (e: MouseEvent) => {
   lastX = e.offsetX;
   lastY = e.offsetY;
   currentPath.push(`M${lastX} ${lastY}`);
-  pathStrings.push(""); // Добавить пустую строку, которая будет заполнена при рисовании
+  pathStrings.push("");
 };
 
 const handleMouseMove = (e: MouseEvent) => {
   if (isDrawing && lastX !== null && lastY !== null) {
-    lastX = lastX + (e.offsetX - lastX) * smoothingFactor;
-    lastY = lastY + (e.offsetY - lastY) * smoothingFactor;
+    let newX = e.offsetX;
+    let newY = e.offsetY;
+
+    lastX = lastX + (newX - lastX) * smoothingFactor;
+    lastY = lastY + (newY - lastY) * smoothingFactor;
+
     currentPath.push(`L${lastX.toFixed(2)} ${lastY.toFixed(2)}`);
-    pathStrings[pathStrings.length - 1] = currentPath.join(" "); // Обновляем последний элемент
+    pathStrings[pathStrings.length - 1] = currentPath.join(" ");
   }
 };
 
 const handleMouseUp = () => {
-  if (isDrawing) {
-    isDrawing = false;
-    lastX = null;
-    lastY = null;
-    currentPath = [];
-  }
+  isDrawing = false;
+  lastX = null;
+  lastY = null;
+  currentPath = [];
 };
 </script>
 
